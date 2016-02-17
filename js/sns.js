8cc1f6721639eaba86cbaf05642cde6b23d6f8f9
yhzion <gplusit@gmail.com>
2016-02-17 18:25:39 +0900

    init

---
+++ js/sns.js
@@ -0,0 +1,20 @@
+$(function(){
+
+
+    var moreLoad = function() {
+        setTimeout(function(){
+            for(var i=0 ; i<5 ; i++) {
+                $('div.items').append($('div.item:first-child').clone().show());
+            }
+        }, 1000);
+    };
+
+    moreLoad();
+
+    $(window).scroll(function(){
+        if  ($(window).scrollTop() == $(document).height() - $(window).height()){
+            moreLoad();
+        }
+    });
+
+});
