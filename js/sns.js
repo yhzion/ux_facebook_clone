3eef4db2989b6041faca21b3f22c67d5ffbe6f8b
yhzion <gplusit@gmail.com>
2016-02-19 18:39:42 +0900

    add code

--- js/sns.js
+++ js/sns.js
@@ -1,12 +1,35 @@
 $(function(){
 
+    var imgIdx = 0;
 
     var moreLoad = function() {
-        setTimeout(function(){
-            for(var i=0 ; i<5 ; i++) {
-                $('div.items').append($('div.item:first-child').clone().show());
-            }
-        }, 300);
+        for(var i=0 ; i<5 ; i++) {
+            $.ajax({
+                url: "http://www.randomtext.me/api/gibberish/p-1/25-75",
+                dataType:"json",
+                data: {
+                    a: 1
+                }
+            }).done(function(result) {
+                var template = $('div.item:first-child').clone().show();
+                template.find('img.scaledImageFitWidth').attr('src','http://lorempixel.com/470/313?a='+ imgIdx++);
+                template.find('.item_body_text').html(result.text_out);
+
+                $.ajax({
+                    url: 'https://randomuser.me/api?a='+i,
+                    dataType: 'json',
+                    success: function(data){
+                        template.find(".profile_name").text(data.results[0].user.name.first);
+                        template.find(".profile_pic img").attr("src",data.results[0].user.picture.thumbnail);
+                        $('div.items').append(template);
+                    }
+                });
+
+
+            });
+
+
+        }
     };
 
     moreLoad();
@@ -14,6 +37,15 @@ $(function(){
     $(window).scroll(function(){
         if  ($(window).scrollTop() == $(document).height() - $(window).height()){
             moreLoad();
+        }
+    });
+
+    $('#chat_header').click(function(){
+        if(!$('chat').hasClass('chaton')) {
+            $('chat').addClass('chaton');
+            $('.text_field').focus();
+        } else {
+            $('chat').removeClass('chaton');
         }
     });
 
