107d84ae59364013a5977b3e6fd80aff5722b664
yhzion <gplusit@gmail.com>
2016-02-23 18:06:51 +0900

    add grunt build

--- src/assets/js/sns.js
+++ src/assets/js/sns.js
@@ -1,8 +1,7 @@
 $(function () {
 
     var random_number = function (min, max) {
-        var num = Math.floor(Math.random() * (max - min)) + min;
-        return num;
+        return Math.floor(Math.random() * (max - min)) + min;
     };
 
 
@@ -20,8 +19,8 @@ $(function () {
                 var template = $('div.item:first-child').clone().show();
                 template.find('img.scaledImageFitWidth').attr('src', 'http://lorempixel.com/470/313?a=' + imgIdx++);
                 template.find('.item_body_text').html(result.text_out);
-                template.find('.item_body_stat span.like').text(random_number(1,500) + " Likes");
-                template.find('.item_body_stat span.share').text(random_number(1,500) + " Share");
+                template.find('.item_body_stat span.like').text(random_number(1, 500) + " Likes");
+                template.find('.item_body_stat span.share').text(random_number(1, 500) + " Share");
                 template.find(".profile_pic img").attr("src", 'http://lorempixel.com/40/40?a=' + imgIdx++);
                 template.find("span.minute").text(imgIdx + " mins");
                 template.find(".profile_name").text(result.text_out.replace(/(<([^>]+)>)/gi, "").split(/\s+/)[0]);
@@ -47,27 +46,28 @@ $(function () {
                 console.log(template);
                 template.find('.ad_group_image img').attr('src', 'http://lorempixel.com/252/152?a=' + imgIdx++);
                 template.find('div.main_title').text(result.text_out.replace(/(<([^>]+)>)/gi, ""));
-                template.find('div.sub_title').text(random_number(1,500) + " members");
+                template.find('div.sub_title').text(random_number(1, 500) + " members");
 
                 $('div.ad_groups').append(template);
             });
         }
-    }
+    };
 
     loadGroups();
 
     $(window).scroll(function () {
-        if($(window).scrollTop() == $(document).height() - $(window).height()){
+        if ($(window).scrollTop() == $(document).height() - $(window).height()) {
             moreLoad();
         }
     });
 
     $('#chat_header').click(function () {
-        if (!$('chat').hasClass('chaton')) {
-            $('chat').addClass('chaton');
+        var chat = $('chat');
+        if (!chat.hasClass('chaton')) {
+            chat.addClass('chaton');
             $('.text_field').focus();
         } else {
-            $('chat').removeClass('chaton');
+            chat.removeClass('chaton');
         }
     });
 
