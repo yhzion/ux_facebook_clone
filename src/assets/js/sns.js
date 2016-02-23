914341095dbf47826fe58796f5ac2876a176e88c
yhzion <gplusit@gmail.com>
2016-02-23 15:17:10 +0900

    add grunt build

--- src/assets/js/sns.js
+++ src/assets/js/sns.js
@@ -1,5 +1,13 @@
 $(function(){
 
+    var random_number = function() {
+        var max = 500;
+        var min = 1;
+        var num = Math.floor(Math.random() * (max - min)) + min;
+        return num;
+    };
+
+
     var imgIdx = 0;
 
     var moreLoad = function() {
@@ -8,12 +16,14 @@ $(function(){
                 url: "https://www.randomtext.me/api/gibberish/p-1/25-75",
                 dataType:"json",
                 data: {
-                    a: 1
+                    a: i
                 }
             }).done(function(result) {
                 var template = $('div.item:first-child').clone().show();
                 template.find('img.scaledImageFitWidth').attr('src','http://lorempixel.com/470/313?a='+ imgIdx++);
                 template.find('.item_body_text').html(result.text_out);
+                template.find('.item_body_stat span.like').text(random_number()+" Likes");
+                template.find('.item_body_stat span.share').text(random_number()+" Share");
 
                 $.ajax({
                     url: 'https://randomuser.me/api?a='+i,
@@ -33,6 +43,28 @@ $(function(){
     };
 
     moreLoad();
+
+    var loadGroups = function() {
+        for(var i=0; i<3 ; i++) {
+            $.ajax({
+                url: "https://www.randomtext.me/api/gibberish/p-1/5-8",
+                dataType:"json",
+                data: {
+                    a: i
+                }
+            }).done(function(result) {
+                var template = $('div.ad_groups div.ad_group:first-child').clone().show();
+                console.log(template);
+                template.find('.ad_group_image img').attr('src','http://lorempixel.com/252/152?a='+ imgIdx++);
+                template.find('div.main_title').text(result.text_out.replace(/(<([^>]+)>)/gi, ""));
+                template.find('div.sub_title').text(random_number() + " members");
+
+                $('div.ad_groups').append(template);
+            });
+        }
+    }
+
+    loadGroups();
 
     $(window).scroll(function(){
         if  ($(window).scrollTop() == $(document).height() - $(window).height()){
